d2054477a29c622b937e2e10aba865bd41fdf921
juanpablobais <juanpablobais@gmail.com>
2023-11-21 21:04:31 -0300

    arreglo imagenes translations

--- src/components/LanguageSelector.js
+++ src/components/LanguageSelector.js
@@ -2,6 +2,8 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import "../style.css"
+import usa from "../Assets/usa.png"
+import argentina from "../Assets/argentina.png"
 
 const LanguageSelector = () => {
   const { i18n } = useTranslation();
@@ -14,14 +16,14 @@ const LanguageSelector = () => {
     <div>
       <button onClick={() => changeLanguage('en')}>
         <img
-          src="https://images.vexels.com/media/users/3/164649/isolated/lists/010f55d9bb5f8a28c3620583482d89ed-circulo-de-icono-de-idioma-de-bandera-de-estados-unidos.png"
+          src={usa}
           alt="English"
           style={{ width: '30px', marginRight: '5px' }}
         />
       </button>
       <button onClick={() => changeLanguage('es')}>
         <img
-          src="https://i.pinimg.com/originals/e5/9b/e8/e59be84c157b5a4af7e1f3709d4aad8d.png"
+          src={argentina}
           alt="Español"
           style={{ width: '30px', marginRight: '5px' }}
         />
